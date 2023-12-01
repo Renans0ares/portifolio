8a6a2f66404b2fdc2a7f032a4c02b968e72aa40f
Renan Soares <renan20012010@outlook.com>
2023-11-30 21:56:00 -0300

    feat: Criado social-section

---
+++ src/app/shared/social-section/social-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-section',
+  templateUrl: './social-section.component.html'
+})
+export class SocialSectionComponent {
+
+}
