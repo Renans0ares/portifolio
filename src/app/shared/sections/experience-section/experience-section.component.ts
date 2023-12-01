6ab54afe1f8718a6199ec22711c95933de9a395b
Renan Soares <renan20012010@outlook.com>
2023-11-30 22:23:04 -0300

    feat: Criado sessao de xp, mas nao implementado

---
+++ src/app/shared/sections/experience-section/experience-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-section',
+  templateUrl: './experience-section.component.html'
+})
+export class ExperienceSectionComponent {
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@
 module.exports = {
   content: [
     "src/app/pages/home/home.component.html",
-    "src/app/shared/btn-primary/btn-primary.component.html",
-    "src/app/shared/github-section/github-section.component.html",
-    "src/app/shared/header/header.component.html",
+    "src/app/shared/sections/github-section/github-section.component.html",
+    "src/app/shared/sections/header/header.component.html",
+    "src/app/shared/sections/main-section/main-section.component.html",
+    "src/app/shared/sections/social-section/social-section.component.html",
+    "src/app/shared/sections/experience-section/experience-section.component.html",
     "src/app/shared/main-picture/main-picture.component.html",
-    "src/app/shared/main-section/main-section.component.html",
-    "src/app/shared/social-section/social-section.component.html",
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
