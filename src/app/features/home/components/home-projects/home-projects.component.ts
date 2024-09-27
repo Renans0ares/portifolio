4a7b75897c753cb15dbcf0d75dad212d1dc4fffa
Renan Sousa <renan20012010@outlook.com>
2024-09-26 22:21:39 -0300

    feat: feito sessao de projetos

---
+++ src/app/features/home/components/home-projects/home-projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-projects.component.html',
+  styleUrl: './home-projects.component.css'
+})
+export class HomeProjectsComponent {
+
+}
