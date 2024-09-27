26fd9502d68977a20eddcc5ebf0897093fd12664
Renan Sousa <renan20012010@outlook.com>
2024-09-26 23:15:16 -0300

    style: Colocando um hover no botao e mudando a cor primária

---
+++ src/app/features/home/components/home-skills/home-skills.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-skills',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-skills.component.html',
+  styleUrl: './home-skills.component.css'
+})
+export class HomeSkillsComponent {
+
+}
