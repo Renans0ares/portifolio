0a7bbee0b27df433709c98a370a624658efca73f
Renan Sousa <renan20012010@outlook.com>
2024-09-26 23:37:19 -0300

    feat: finalizado os components do home - em breve será modificado

---
+++ src/app/features/home/components/home-contacts/home-contacts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-contacts',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-contacts.component.html',
+  styleUrl: './home-contacts.component.css'
+})
+export class HomeContactsComponent {
+
+}
