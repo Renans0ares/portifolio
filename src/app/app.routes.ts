5cf313016f35d751dc08e05dc469228e33eb3421
Renan Sousa <renan.sousa@datasystem.com.br>
2024-10-22 15:14:48 -0300

    fix: Resolvido problema de roteamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/pages/home/home.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ContactsComponent } from './features/contacts/contacts.component';
+import { AboutComponent } from './features/about/about.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contacts', component: ContactsComponent },
+    { path: 'about', component: AboutComponent }
 ];
