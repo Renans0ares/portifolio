54b469025e80b9da9745e87148abe577a9f92ce0
Renan Sousa <renan.sousa@datasystem.com.br>
2024-10-22 20:53:33 -0300

    feat: aplicando responsividade

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -9,5 +9,10 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    if (navLinks) { // Verifica se navLinks não é nulo
+      navLinks.classList.toggle('active'); // Adiciona ou remove a classe 'active'
+    } // Adiciona ou remove a classe 'active'
+  }
 }
